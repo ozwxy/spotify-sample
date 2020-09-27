c622910b545fbd7a752f7eb5397eefb7a1a87bf4
ozwxy <ozwio.me@gmail.com>
2020-09-27 09:21:28 +0900

    move

---
+++ spotify-sample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  spotify-sample.h
+//  spotify-sample
+//
+//  Created by Kodai Ozawa on 2020/09/26.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#ifndef spotify_sample_h
+#define spotify_sample_h
+
+
+#endif /* spotify_sample_h */
